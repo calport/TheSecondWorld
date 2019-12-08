81a5ec4471a198f837169a568975f25ade5a5bd2
Bri <bs3864@nyu.edu>
2019-12-07 22:57:53 -0500

    Fixed a small error due to auto complete

--- thesisTest/Assets/Scripts/Video/VideoManager.cs
+++ thesisTest/Assets/Scripts/Video/VideoManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
@@ -6,6 +7,7 @@ using UnityEngine.Video;
 public class VideoManager : MonoBehaviour
 {
     public GameObject[] VideoPlanes;
+    public FireRoom currentFireRoomState;
     
     // enum with ISFIREACCESSABLE FIRESIDEB-NOT FIRESIDEA_NOT
     public enum StairRoom
@@ -29,6 +31,28 @@ public class VideoManager : MonoBehaviour
         //this is Preparing your videos for the game 
         //by preloading them all so you will not have buffer problems
         PreLoadVideos();
+    }
+
+    public void Start()
+    {
+        currentFireRoomState = FireRoom.idleVideo;
+    }
+
+    public void Update()
+    {
+        switch (currentFireRoomState)
+        {
+            case FireRoom.VideoA:
+                break;
+            case FireRoom.VideoB:
+                break;
+            case FireRoom.idleVideo:
+                break;
+            case FireRoom.isFinished:
+                break;
+            default:
+                break;
+        }
     }
 
     private void PreLoadVideos()
@@ -57,15 +81,21 @@ public class VideoManager : MonoBehaviour
         
     }
 
-    public  CheckFireRoomAvailablity(GameObject StateA, GameObject StateB, GameObject StateC) //this function see's what options are available 
+    public void ChangeFireRoomState(FireRoom newState)
     {
-        if (ServiceLocator.FireRoomA && ServiceLocator.FireRoomB && ServiceLocator.FireRoomC) return StateA; 
-        else if (!ServiceLocator.FireRoomA && ServiceLocator.FireRoomB) return StateB; 
-        else if (ServiceLocator.FireRoomA && !ServiceLocator.FireRoomB) return StateA;
-        else return StateC; 
-            
-        
-        return null; 
+        currentFireRoomState = newState; 
+    }
+
+    public void CheckFireRoomAvailablity(GameObject StateA, GameObject StateB, GameObject StateC) //this function see's what options are available 
+    {
+
+        if (ServiceLocator.FireRoomA && ServiceLocator.FireRoomB && ServiceLocator.FireRoomC)
+            currentFireRoomState = FireRoom.VideoA; 
+        else if (!ServiceLocator.FireRoomA && ServiceLocator.FireRoomB) 
+            currentFireRoomState = FireRoom.VideoB;
+        else if (ServiceLocator.FireRoomA && !ServiceLocator.FireRoomB) 
+            currentFireRoomState = FireRoom.VideoA;
+        else  currentFireRoomState = FireRoom.idleVideo;
     }
   
 }
