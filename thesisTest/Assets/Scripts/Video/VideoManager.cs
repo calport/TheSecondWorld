475fce41553d7a4cab837ec36cf7d4c1d4f165fb
Bri <bs3864@nyu.edu>
2019-12-07 20:33:26 -0500

    Added Preload Feature & ServiceLocator

---
+++ thesisTest/Assets/Scripts/Video/VideoManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+    public GameObject[] VideoPlanes;
+
+    void Awake()
+    {
+        //this is Preparing your videos for the game 
+        //by preloading them all so you will not have buffer problems
+        PreLoadVideos();
+    }
+
+    private void PreLoadVideos()
+    {
+        for (int i = 0; i >= VideoPlanes.Length; i++)
+        {
+            VideoPlanes[i].GetComponent<VideoPlayer>().Prepare();
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
